a26461af9a829859f36bb29546946eed4ccad612
Yizhak Shenk <1997shenk@gmail.com>
2023-04-25 16:08:10 +0300

    marge-master

--- client/src/componets/notifications/Notification.jsx
+++ client/src/componets/notifications/Notification.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import axios from 'axios'
-import {urlServer} from '../../utilities/Url/url'
-import { Box, ListItem,  ListItemText, Typography, Divider } from '@mui/material'
+import React from "react";
+import axios from "axios";
+import { urlServer } from "../../utilities/Url/url";
+import {
+  Box,
+  ListItem,
+  ListItemText,
+  Typography,
+  Divider,
+  ListItemButton,
+} from "@mui/material";
 
 export default function Notification({ notification }) {
-    const handleClick = async () => {
-        await axios.put(`${urlServer}/notification/update`,{id:notification.id, has_readed:true});
-        window.open(notification.url)
-    }
-    return (
-        <Box onClick={handleClick}>
-            <ListItem disablePadding>
-                <ListItemText sx={{ textAlign: 'center' }}>
-                    <Typography >
-                        {notification.message}
-                    </Typography>
-                </ListItemText>
-            </ListItem>
-            <Divider />
-        </Box>
-    )
+  const handleClick = async () => {
+    await axios.put(`${urlServer}/notification/update`, {
+      id: notification.id,
+      has_readed: true,
+    });
+    window.open(notification.url);
+  };
+  return (
+    <Box onClick={handleClick}>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemText sx={{ textAlign: "center" }}>
+            <Typography>{notification.message}</Typography>
+          </ListItemText>
+        </ListItemButton>
+      </ListItem>
+      <Divider />
+    </Box>
+  );
 }
